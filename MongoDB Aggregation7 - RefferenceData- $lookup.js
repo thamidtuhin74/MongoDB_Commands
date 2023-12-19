55f3036609b6b209c39e9803b63b4d7c26bb37e9
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-12-19 13:36:16 +0600

    MongoDB Aggregation7 - RefferenceData-

---
+++ MongoDB Aggregation7 - RefferenceData- $lookup.js
@@ -0,0 +1,20 @@
+//MongoDB Aggregation7 - RefferenceData- $lookup
+// embedded data vs Refference Data
+// Syntax
+// $lookup: {
+//               from: "<collection to join>",
+//               localField: "<field from the input documents>",
+//               foreignField: "<field from the documents of the from collection>",
+//               as: "<output array field>"
+//              }
+db.orders.aggregate([
+    
+    {
+        $lookup: { //referencing data to another collection & get full data
+               from: "test",
+               localField: "userId",
+               foreignField: "_id",
+               as: "user"
+             }
+    }
+    ])
